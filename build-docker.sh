3a464c7c246b0e2a6c99ecaf33af868712679ca7
Dr David Race <drace.optimizer@gmail.com>
2019-05-03 16:13:37 -0500

    Semi-auto build

--- build-docker.sh
+++ build-docker.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
-docker build -f ./dockerfile -t python-run:latest .
+basename="python"
+runname="${basename}-run"
+echo "updating ${runname}"
+
+desimages=$(./docker_descendants.py ${basename} | awk '{print $1}')
+for imag in ${desimages}
+do
+	echo "Removing docker image ${imag}"
+	docker image rm -f ${imag}
+done
+docker image rm -f ${runname}
+docker pull ${basename}
+docker build -f ./dockerfile -t ${runname} .
